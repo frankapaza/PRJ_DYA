64fce3501ff3cb35216d3eda2a5eb76fd75223c6
Jose André Miralles Delgado <miralles.jose.18@gmail.com>
2020-04-13 16:25:18 -0500

    Commit #1 - Primeros cambios - Portal, Registro usuario y nuevos archivos.

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DYA
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Seguridad", action = "IngresoSistema", id = UrlParameter.Optional }
+                defaults: new { controller = "Portal", action = "ContenidoPortal", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/PortalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DYA.Controllers
+{
+    public class PortalController : Controller
+    {
+        // GET: Portal
+        public ActionResult ContenidoPortal()
+        {
+            return View();
+        }
+    }
+}
